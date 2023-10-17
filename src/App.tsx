64ab4aceea8508445d845ad0551cd0e99adec4db
raza <razakhanha05@gmail.com>
2023-10-17 20:16:26 +0530

    em

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Route, useHistory } from "react-router-dom";
 import { HomePage } from "./Pages/HomePage";
 import { ChatPage } from "./Pages/ChatPage";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { MainPage } from "./Components/test/MainPage";
 function App() {
-  const history = useHistory();
-  useEffect(() => {
-    const loggedUser = localStorage.getItem("userInfo");
-    if (!loggedUser) {
-      history.push("/");
-    }
-  }, []);
+  // const history = useHistory();
+  // useEffect(() => {
+  //   const loggedUser = localStorage.getItem("userInfo");
+  //   if (!loggedUser) {
+  //     history.push("/");
+  //   }
+  // }, []);
   return (
     <div className="h-[100vh] font-mono w-full  text-gray-400 bg-base-300">
       <Route path="/" exact component={HomePage} />
