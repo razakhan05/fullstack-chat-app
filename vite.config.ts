3d0d881a187ea136f13b36bcf0cf7af9b287aa66
raza <razakhanha05@gmail.com>
2023-10-17 20:00:15 +0530

    em

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {},
+  server: {
+    proxy: {
+      "/api": "https://chitchat-server-6cet.onrender.com",
+    },
+  },
   plugins: [react()],
 });
