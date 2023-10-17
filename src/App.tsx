f4e672e39e8f330a32d568269bd2463e8725994e
raza <razakhanha05@gmail.com>
2023-10-17 19:04:31 +0530

    rem

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, useHistory } from "react-router-dom";
 import { HomePage } from "./Pages/HomePage";
 import { ChatPage } from "./Pages/ChatPage";
 import { useEffect } from "react";
+import { MainPage } from "./Components/test/MainPage";
 function App() {
   const history = useHistory();
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
     <div className="h-[100vh] font-mono w-full  text-gray-400 bg-base-300">
       <Route path="/" exact component={HomePage} />
       <Route path="/chats" component={ChatPage} />
+      <Route path="/main" component={MainPage} />
     </div>
   );
 }
